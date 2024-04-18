ed5df6d52722480d628c6f61087f617718c27702
Kirit Nakka <kiritnakka@Music-M1.local>
2024-04-18 19:36:19 -0400

    Added form functionality and cleaned up UI/UX

--- scripts.js
+++ scripts.js
@@ -7,3 +7,18 @@ window.onscroll = function() {
         header[0].classList.remove("active-header")
     }
 }
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevents the default form submission
+  
+    // Create a FormData object, which is not used here but can be used to send data to a server
+    var formData = new FormData(event.target);
+  
+    // Retrieve the modal instance and hide the modal
+    var contactModal = bootstrap.Modal.getInstance(document.getElementById('contactModal'));
+    contactModal.hide();
+  
+    // Clear the form fields
+    event.target.reset();
+  });
+  
