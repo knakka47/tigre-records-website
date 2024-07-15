a1c2477c7545fa700ea7b68304fc09468a82ac19
Kirit Nakka <kiritnakka@music-m1.mynetworksettings.com>
2024-07-15 19:53:59 -0400

    Updated artist pics, still developing blog

--- scripts.js
+++ scripts.js
@@ -15,28 +15,25 @@ document.addEventListener('DOMContentLoaded', function () {
         var formData = new FormData(event.target);
         var contactModal = bootstrap.Modal.getInstance(document.getElementById('contactModal'));
 
-        // Use Fetch API to submit the form data to Google Apps Script
-        fetch('https://script.google.com/macros/s/AKfycbzH7Gl1lVi0swEoOMfrjdkqPk4hoB3H2tr_Ad0d7eouPWr8Wc2BaEttBAs1cwgLL07b/exec', {
+        // Use Fetch API to submit the form data to Web3Forms
+        fetch(event.target.action, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(Object.fromEntries(formData.entries()))
+            body: formData
         })
-            .then(response => response.json()) // Assuming response is JSON
-            .then(data => {
-                var successMessage = document.getElementById('successMessage');
-                successMessage.textContent = "Thank you for your interest. We will get back to you soon!";
-                successMessage.style.display = 'block'; // Make the success message visible
+        .then(response => response.json()) // Assuming response is JSON
+        .then(data => {
+            var successMessage = document.getElementById('successMessage');
+            successMessage.textContent = "Thank you for your interest in booking studio time! We will get back to you soon!";
+            successMessage.style.display = 'block'; // Make the success message visible
 
-                setTimeout(() => {
-                    contactModal.hide(); // Hide the modal
-                    successMessage.style.display = 'none'; // Hide the success message for future use
-                }, 5000); // Adjust time as needed
+            setTimeout(() => {
+                contactModal.hide(); // Hide the modal
+                successMessage.style.display = 'none'; // Hide the success message for future use
+            }, 5000); // Adjust time as needed
 
-                // Clear the form fields
-                event.target.reset();
-            })
-            .catch(error => console.error('Error:', error));
+            // Clear the form fields
+            event.target.reset();
+        })
+        .catch(error => console.error('Error:', error));
     });
 });
